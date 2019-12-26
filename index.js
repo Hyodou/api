da4b8951a96aacc19f1c801f5d153a7b7ef13e87
Rdea <victor160794@gmail.com>
2019-12-26 16:21:44 -0500

    form

---
+++ index.js
@@ -0,0 +1,423 @@
+const mysql = require('mysql');
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const bodyparser = require('body-parser');
+const hbs = require('hbs');
+const Handlebars = require('express-handlebars');
+var app = express();
+//Configuring express server
+app.set('views',path.join(__dirname,'views'));
+//set view engine
+app.set('view engine', 'hbs');
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use('/assets',express.static(__dirname + '/public'));
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+//database
+const http = require('http');
+
+const hostname = '127.0.0.1';
+var mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ctapp',
+  multipleStatements: true
+  });
+mysqlConnection.connect((err)=> {
+  if(!err)
+  console.log('Connection Established Successfully');
+  else
+  console.log('Connection Failed!'+ JSON.stringify(err,undefined,2));
+  });
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`Listening on port ${port}..`));
+
+//user
+app.get('/user' , (req, res) => {
+  let sql = "SELECT * FROM user";
+  let query = mysqlConnection.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('user_view',{
+      results: results
+    });
+  });
+});
+  
+app.post('/user/save',(req, res) => {
+  let data = {cedula: req.body.cedula, nombre: req.body.nombre, apellido: req.body.apellido, email: req.body.email, telefono: req.body.telefono};
+  let sql = "INSERT INTO user SET ?";
+  let query = mysqlConnection.query(sql, data,(err, results) => {
+    if(err) throw err;
+    res.redirect('/user');
+  });
+});
+
+app.post('/user/update',(req, res) => {
+  let sql = "UPDATE user SET nombre='"+req.body.nombre+"', apellido='"+req.body.apellido+"', email='"+req.body.email+"', telefono='"+req.body.telefono+"' WHERE cedula="+req.body.cedula;
+  let query = mysqlConnection.query(sql, (err, results) => {
+    if(err) throw err;
+    res.redirect('/user');
+  });
+});
+
+
+//administrador  
+app.get('/administradores' , (req, res) => {
+  let sql = "select idadministrador, cedula, nombre, apellido, email, telefono, contraseñaadministrador from user u, administrador a where u.cedula = a.userid";
+  let query = mysqlConnection.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('administrador_view',{
+      results: results
+    });
+  });
+});
+
+app.post('/administradores/save',(req, res) => {
+  let data = {cedula: req.body.cedula, nombre: req.body.nombre, apellido: req.body.apellido, email: req.body.email, telefono: req.body.telefono};
+  let sql = "INSERT INTO user SET ?";
+  let query = mysqlConnection.query(sql, data,(err, results) => {
+    if(err) throw err;
+    let data2 = {userid: req.body.cedula, contraseñaadministrador: req.body.contraseñaadministrador};
+    let sql2 = "INSERT INTO administrador SET ?";
+    let q2 = mysqlConnection.query(sql2, data2, (err2, results)=>{
+      if(err2) throw err2;
+    });
+    res.redirect('/administradores');
+  });
+}); 
+
+app.post('/administradores/update',(req, res) => {
+  let sql2 = "UPDATE user SET nombre='"+req.body.nombre+"', apellido='"+req.body.apellido+"', email='"+req.body.email+"', telefono='"+req.body.telefono+"' WHERE cedula="+req.body.cedula;
+  let query = mysqlConnection.query(sql2, (err, results) => {
+    if(err) throw err;
+    let sql = "UPDATE administrador SET contraseñaadministrador='"+req.body.contraseñaadministrador+"' WHERE userid="+req.body.cedula;
+    let query2 = mysqlConnection.query(sql, (err, results) =>{
+      if(err) throw err;
+    });
+    res.redirect('/administradores');
+  });
+});
+
+
+//cliente  
+app.get('/clientes' , (req, res) => {
+  let sql = "select idcliente, cedula, nombre, apellido, email, telefono, direccion from user u, cliente a where u.cedula = a.userid";
+  let query = mysqlConnection.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('cliente_view',{
+      results: results
+    });
+  });
+});
+
+app.post('/clientes/save',(req, res) => {
+  let data = {cedula: req.body.cedula, nombre: req.body.nombre, apellido: req.body.apellido, email: req.body.email, telefono: req.body.telefono};
+  let sql = "INSERT INTO user SET ?";
+  let query = mysqlConnection.query(sql, data,(err, results) => {
+    if(err) throw err;
+    let data2 = {userid: req.body.cedula, direccion: req.body.direccion};
+    let sql2 = "INSERT INTO cliente SET ?";
+    let q2 = mysqlConnection.query(sql2, data2, (err2, results)=>{
+      if(err2) throw err2;
+    });
+    res.redirect('/clientes');
+  });
+});
+
+
+app.post('/clientes/update',(req, res) => {
+  let sql2 = "UPDATE user SET nombre='"+req.body.nombre+"', apellido='"+req.body.apellido+"', email='"+req.body.email+"', telefono='"+req.body.telefono+"' WHERE cedula="+req.body.cedula;
+  let query = mysqlConnection.query(sql2, (err, results) => {
+    if(err) throw err;
+    let sql = "UPDATE cliente SET direccion='"+req.body.direccion+"' WHERE userid="+req.body.cedula;
+    let query2 = mysqlConnection.query(sql, (err, results) =>{
+      if(err) throw err;
+    });
+    res.redirect('/clientes');
+  });  
+});
+
+
+
+//vendedor
+app.get('/vendedores' , (req, res) => {
+  let sql = "select idvendedor, cedula, nombre, apellido, email, telefono, tipoid, nombretipovendedor, contraseñavendedor from user u, vendedor a, tipovendedor t where u.cedula = a.userid and a.tipoid = t.idtipovendedor";
+  let query = mysqlConnection.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('vendedor_view',{
+      results: results
+    });
+  });
+});
+
+app.post('/vendedores/save',(req, res) => {
+  let data = {cedula: req.body.cedula, nombre: req.body.nombre, apellido: req.body.apellido, email: req.body.email, telefono: req.body.telefono};
+  let sql = "INSERT INTO user SET ?";
+  let query = mysqlConnection.query(sql, data,(err, results) => {
+    if(err) throw err;
+    let data2 = {userid: req.body.cedula, contraseñavendedor: req.body.contraseñavendedor};
+    let sql2 = "INSERT INTO vendedor SET ?";
+    let q2 = mysqlConnection.query(sql2, data2, (err2, results)=>{
+      if(err2) throw err2;
+    });
+    res.redirect('/vendedores');
+  });
+});
+
+app.post('/vendedores/update',(req, res) => {
+  let sql2 = "UPDATE user SET nombre='"+req.body.nombre+"', apellido='"+req.body.apellido+"', email='"+req.body.email+"', telefono='"+req.body.telefono+"' WHERE cedula="+req.body.cedula;
+  let query = mysqlConnection.query(sql2, (err, results) => {
+    if(err) throw err;
+    let sql = "UPDATE vendedor SET contraseñavendedor='"+req.body.contraseñavendedor+"', tipoid='"+req.body.tipoid+"' WHERE userid="+req.body.cedula;
+    let query2 = mysqlConnection.query(sql, (err, results) =>{
+      if(err) throw err;
+    });
+    res.redirect('/vendedores');
+  });
+});
+
+
+//asesor  
+app.get('/asesores' , (req, res) => {
+  let sql = "select idasesor, cedula, nombre, apellido, email, telefono, contraseñaasesor from user u, asesor a where u.cedula = a.userid";
+  let query = mysqlConnection.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('asesor_view',{
+      results: results
+    });
+  });
+});
+
+app.post('/asesores/save',(req, res) => {
+  let data = {cedula: req.body.cedula, nombre: req.body.nombre, apellido: req.body.apellido, email: req.body.email, telefono: req.body.telefono};
+  let sql = "INSERT INTO user SET ?";
+  let query = mysqlConnection.query(sql, data,(err, results) => {
+    if(err) throw err;
+    let data2 = {userid: req.body.cedula, contraseñaasesor: req.body.contraseñaasesor};
+    let sql2 = "INSERT INTO asesor SET ?";
+    let q2 = mysqlConnection.query(sql2, data2, (err2, results)=>{
+      if(err2) throw err2;
+    });
+    res.redirect('/asesores');
+  });
+});
+
+app.post('/asesores/update',(req, res) => {
+  let sql2 = "UPDATE user SET nombre='"+req.body.nombre+"', apellido='"+req.body.apellido+"', email='"+req.body.email+"', telefono='"+req.body.telefono+"' WHERE cedula="+req.body.cedula;
+  let query = mysqlConnection.query(sql2, (err, results) => {
+    if(err) throw err;
+    let sql = "UPDATE asesor SET contraseñaasesor='"+req.body.contraseñaasesor+"' WHERE userid="+req.body.cedula;
+    let query2 = mysqlConnection.query(sql, (err, results) =>{
+      if(err) throw err;
+    });
+    res.redirect('/asesores');
+  }); 
+});
+
+
+//registro
+app.get('/confirmaciones', (req, res) =>{
+  let sql = "select id, userid, nombrestadoventa as estado, nombre, apellido, email, telefono, direccion from registro r, cliente c, user u, estadoventa e where r.clienteid = c.userid and c.userid = u.cedula and r.estadoid = e.idestadoventa and r.estadoid = 1";
+  let query = mysqlConnection.query(sql, (err, results) =>{
+    if(err) throw err;
+    res.render('confirmacion_view', {
+      results: results
+    });
+  });
+});
+
+app.post('/confirmaciones/update', (req, res) =>{
+  let sql = "UPDATE registro SET  estadoid='"+req.body.estado+"' WHERE id="+req.body.id;
+  let query = mysqlConnection.query(sql, (err, results) => {
+    if(err) throw err;
+    res.redirect('/confirmaciones');
+  });
+});
+
+app.get('/asignaciones' , (req, res) => {
+  let sql = "select clienteid, userid, nombrestadoventa as estado, nombre, apellido, email, telefono, direccion from registro r, cliente c, user u, estadoventa e where r.clienteid = c.userid and c.userid = u.cedula and r.estadoid = e.idestadoventa and r.estadoid = 2";
+  let query = mysqlConnection.query(sql, (err, results) => {
+    if(err) throw err;
+    let sql2 = "select idasesor, nombre as nombreasesor, apellido as apellidoasesor from asesor a, user u where a.userid = u.cedula";
+    let query2 = mysqlConnection.query(sql2, (err, results2) =>{
+      if(err) throw err;
+        res.render('asignacion_view',{
+          results: results, results2: results2
+        });
+    }); 
+  });
+});
+
+app.get('/registro' , (req, res) => {
+  let sql = "select id, userid, vendedorid, nombrestadoventa as estado, nombre, apellido, email, telefono, direccion from registro r, cliente c, user u, estadoventa e where r.clienteid = c.userid and c.userid = u.cedula and r.estadoid = e.idestadoventa";
+  let query = mysqlConnection.query(sql, (err, results) =>{
+    if(err) throw err;
+    let sql2 = "select idvendedor, nombre as nombrevendedor, apellido as apellidovendedor from vendedor";
+    let query2 = mysqlConnection.query(sql2, (err, results2) =>{
+      res.render('registro_view', {
+        results: results, results2: results2
+      });
+    });
+  });
+});
+
+app.post('/asignaciones/save',(req, res) => {
+  let data = {clienteid: req.body.clienteid, asesorid: req.body.asesorid};
+  let sql = "INSERT INTO asignacion SET ?";
+  let query = mysqlConnection.query(sql, data,(err, results) => {
+    if(err) throw err;
+  });
+  let sql2 = "UPDATE registro SET estadoid='3'";
+  let query2 = mysqlConnection.query(sql2,(err,results)=>{
+    if(err) throw err;
+    res.redirect('/asignaciones');
+  });
+});
+
+app.post('/registro/save',(req, res) => {
+  let data = {clienteid: req.body.clienteid, vendedorid: req.body.vendedorid};
+  let sql = "INSERT INTO registro SET ?";
+  let query = mysqlConnection.query(sql, data,(err, results) => {
+    if(err) throw err;
+    res.redirect('/registro');
+  });
+});
+
+app.post('/registro/update',(req, res) => {
+  let sql = "UPDATE registro SET estadoid='"+req.body.estadoid+"' WHERE id="+req.body.id;
+  let query = mysqlConnection.query(sql, (err, results) => {
+    if(err) throw err;
+    res.redirect('/registro');
+  });
+});
+
+app.post('/registro/updatev',(req, res) => {
+  let sql = "UPDATE registro SET valor='"+req.body.valor+"' WHERE id="+req.body.id;
+  let query = mysqlConnection.query(sql, (err, results) => {
+    if(err) throw err;
+    res.redirect('/registro');
+  });
+});
+
+app.post('/registro/updateall', (req, res) =>{
+  let sql2 = "UPDATE user SET nombre='"+req.body.nombre+"', apellido='"+req.body.apellido+"', email='"+req.body.email+"', telefono='"+req.body.telefono+"' WHERE cedula="+req.body.userid;
+  let query = mysqlConnection.query(sql2, (err, results) => {
+    if(err) throw err;
+    let sql = "UPDATE cliente SET direccion='"+req.body.direccion+"' WHERE userid="+req.body.userid;
+    let query2 = mysqlConnection.query(sql, (err, results) =>{
+      if(err) throw err;
+    });
+    res.redirect('/registro');
+  }); 
+});
+app.post('/vregistro/updateall', (req, res) =>{
+  let sql2 = "UPDATE user SET nombre='"+req.body.nombre+"', apellido='"+req.body.apellido+"', email='"+req.body.email+"', telefono='"+req.body.telefono+"' WHERE cedula="+req.body.userid;
+  let query = mysqlConnection.query(sql2, (err, results) => {
+    if(err) throw err;
+    let sql = "UPDATE cliente SET direccion='"+req.body.direccion+"' WHERE userid="+req.body.userid;
+    let query2 = mysqlConnection.query(sql, (err, results) =>{
+      if(err) throw err;
+    });
+    res.redirect('/vregistro/'+req.body.vendedorid);
+  }); 
+});
+
+// registro para vendedor
+app.get('/vregistro/:id', (req, res) =>{
+  var user;
+  var idv;
+  let usql = "select idvendedor, nombre, apellido from user u, vendedor v where v.userid = u.cedula and v.idvendedor = "+req.params.id;
+  let uquery = mysqlConnection.query(usql, (uerr, uresults) =>{
+    if(uerr) throw uerr;
+    user = ""+uresults[0].nombre+" "+uresults[0].apellido;
+    idv = uresults[0].idvendedor;
+  });
+  let sql = "select userid, valor, vendedorid, nombrestadoventa as estado, nombre, apellido, email, telefono, direccion from registro r, cliente c, user u, estadoventa e where r.clienteid = c.userid and c.userid = u.cedula and r.estadoid = e.idestadoventa and vendedorid = "+req.params.id;
+  let query = mysqlConnection.query(sql, (err, results) =>{
+    if(err) throw err;  
+    res.render('vregistro_view', {
+       results: results,
+       user: user,
+       idv: idv
+    });
+  });
+});
+
+app.post('/vregistro/save/', (req, res) =>{
+  let data = {cedula: req.body.cedula, nombre: req.body.nombre, apellido: req.body.apellido, email: req.body.email, telefono: req.body.telefono};
+  let sql = "INSERT INTO user SET ?";
+  const vendedorid = req.body.vendedorid;
+  const clienteid = req.body.cedula;
+  let query = mysqlConnection.query(sql, data,(err, results) => {
+    if(err) throw err;
+    let data2 = {userid: req.body.cedula, direccion: req.body.direccion};
+    let sql2 = "INSERT INTO cliente SET ?";
+    let q2 = mysqlConnection.query(sql2, data2, (err2, results2)=>{
+      if(err2) throw err2;
+      let data3 = {clienteid, vendedorid};
+      let sql3 = "INSERT INTO registro SET ?";
+      let qc = mysqlConnection.query(sql3, data3, (err3, results3) =>{
+        if(err3) throw err3;
+      });
+    });
+    res.redirect('/vregistro/'+req.body.vendedorid);
+  });
+});
+
+app.get('/', (req, res) =>{
+  res.render('login_view', {
+    
+  });
+});
+
+app.post('/login', (req, res) =>{
+  var username = req.body.cedula;
+  var password = req.body.contraseña;
+  if(username && password){
+    mysqlConnection.query('SELECT * FROM vendedor WHERE userid = ? AND contraseñavendedor = ?', [username, password], function(err, results2){
+      if(err) throw err;
+      var leng = results2.length;
+      if(leng > 0){
+        req.session.loggedin = true;
+        req.session.username = username;
+        var un = results2[0].idvendedor;
+        var dir = "/vregistro/"+un;
+        res.redirect(dir);
+      }else{
+        res.send('Nombre de Usuario o Contraseña Incorrecto');
+      }
+      res.end();
+    });
+  }else{
+    res.send('Ingrese Nombre de Usuario y Contraseña');
+    res.end();
+  }
+});
+
+app.get('/logout',  (req, res) =>{
+  req.session.loggedin = false;
+  res.redirect('/');
+});
+
+app.get('/register', (req, res) =>{
+  res.render('reg_view', {
+    
+  });
+});
+
+// registro para asesores
+app.get('/aregistro/:id', (req, res) =>{
+  let sql = "select id, userid, valor, nombre, apellido, email, telefono, direccion from asignacion a, registro r, cliente c, user u where a.registroid = r.id and r.clienteid = c.userid and c.userid = u.cedula and asesorid = "+req.params.id;
+  let query = mysqlConnection.query(sql, (err, results) =>{
+    if(err) throw err;  
+    res.render('aregistro_view', {
+       results: results
+    });
+  });
+});
